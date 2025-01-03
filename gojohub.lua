4575076058620ce6904cc0d84866aaa9e0a8c423
Kaymoigame <159413251+TheAnhjklo@users.noreply.github.com>
2025-01-03 19:11:02 +0700

    Add files via upload

---
+++ gojohub.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Gojo Hub",--Tên Script
+    logoscript = "87846504032071",--ID Logo Script
+    tacgia = "Nguyễn Thế Anh"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
